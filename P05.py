f4c4deb29b49478821c2021571f73869d4ffff1d
youngsoo <pullinear@daum.net>
2019-11-20 13:56:48 +0900

    prev solving doesn't consider number of some coin is 0

--- P05.py
+++ P05.py
@@ -9,13 +9,10 @@ coin = [500, 100, 50, 10]
 def coin_combination(n, coin_list, use_list, level):
     way = 0
     copy_list = use_list.copy()
-
     while(True) :
-        copy_list[level] += 1
         sum = 0;
         for i in range(len(coin_list)):
             sum += coin_list[i] * copy_list[i]
-
         if sum == n:
             print(copy_list)
             return way+1
@@ -24,7 +21,7 @@ def coin_combination(n, coin_list, use_list, level):
         else:
             if level < 3:
                 way += coin_combination(n, coin_list, copy_list, level+1)
-
+        copy_list[level] += 1
     return way
 
 print(coin_combination(1000, coin, [0,0,0,0], 0))
